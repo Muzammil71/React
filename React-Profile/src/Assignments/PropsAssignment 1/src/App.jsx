4c3ee24836c6a930677bd3ec8e4db380b61457d8
Muzammil71 <zamiikhan713@gmail.com>
2024-06-04 16:44:56 -0700

    Car Properties 1

---
+++ React-Profile/src/Assignments/PropsAssignment 1/src/App.jsx
@@ -0,0 +1,12 @@
+import Car from "./components/components";
+    function App() {
+    return (
+    <>
+    <Car 
+    name = "Civic X"
+    color = "Black"
+    company ="Honda"/>
+    </>
+    );
+}
+export default App;
---
+++ React-Profile/src/Assignments/PropsAssignment 1/src/components/components.jsx
@@ -0,0 +1,11 @@
+function Car({name,color,company}){
+    return(
+    <>
+    <h1>Car Properties 1</h1>
+    <p>Name : {name}</p>
+    <p>Color : {color}</p>
+    <p>Company : {company}</p>
+    </>
+    );
+}
+export default Car;
