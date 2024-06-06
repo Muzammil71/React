833c49c081880a109802280e6aba7562d6db98f7
Muzammil71 <zamiikhan713@gmail.com>
2024-06-06 15:24:32 -0700

    Props

---
+++ PropsAssignment/Prop/src/App.jsx
@@ -0,0 +1,12 @@
+import Prop1 from "./Component/Prop1";
+function App1() {
+  return(
+    <>
+    <Prop1
+    Name = "CivicX"
+    Color = "Black"
+    Make = "Honda"/>
+    </>
+  );
+}
+export default App1;
---
+++ PropsAssignment/Prop/src/Component/Prop1.jsx
@@ -0,0 +1,11 @@
+function Prop1({Name,Color,Make}) {
+    return(
+    <>
+    <h1>Car Properties</h1>
+    <p>Name : {Name}</p>
+    <p>Color : {Color}</p>
+    <p>Make : {Make}</p>
+    </>
+    );
+}
+export default Prop1;
